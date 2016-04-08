7ac8acb1a1d2330ff028b20e9d9fb92d3f6f1a3f
Yan-Yi Li <liyanyi@gmail.com>
2016-04-08 23:50:42 +0100

    add test for aged brie

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,23 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  subject(:gilded_rose) { described_class.new(items) }
+  let(:items) { [
+    Item.new("foo", 0, 0),
+    Item.new("Aged Brie", 2, 0)
+    ] }
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      gilded_rose.update_quality()
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    context "Aged Brie" do
+      it "increases in Quality the older it gets" do
+        gilded_rose.update_quality()
+        expect(items[1].quality).to eq 1
+      end
+    end
+  end
 end
