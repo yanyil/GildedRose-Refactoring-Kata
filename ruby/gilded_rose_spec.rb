6e2d1d6d549efc75e83c26bd7e8bfbcc2da46071
Yan-Yi Li <liyanyi@gmail.com>
2016-04-10 18:46:50 +0100

    add test for Sulfuras

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -4,19 +4,27 @@ describe GildedRose do
   subject(:gilded_rose) { described_class.new(items) }
   let(:items) { [
     Item.new("foo", 0, 0),
-    Item.new("Aged Brie", 2, 0)
+    Item.new("Aged Brie", 2, 0),
+    Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
     ] }
 
   describe "#update_quality" do
     it "does not change the name" do
-      gilded_rose.update_quality()
+      gilded_rose.update_quality
       expect(items[0].name).to eq "foo"
     end
 
     context "Aged Brie" do
       it "increases in Quality the older it gets" do
-        gilded_rose.update_quality()
+        gilded_rose.update_quality
         expect(items[1].quality).to eq 1
+      end
+    end
+
+    context "Sulfuras, Hand of Ragnaros" do
+      it "does not decrease in Quality" do
+        gilded_rose.update_quality
+        expect(items[2].quality).to eq 80
       end
     end
   end
